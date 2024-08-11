fc8645043307d45c27158f0fb545f150dcad6871
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-08-11 19:51:40 +0530

    add strings file

---
+++ lib/utils/constant/strings.dart
@@ -0,0 +1,6 @@
+class Strings {
+  static const String signIn = "Sign In";
+  static const String password = 'Password';
+  static const String email = 'Email';
+  static const String home = 'Home';
+}
